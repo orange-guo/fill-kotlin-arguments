0c77950508069308b598e57966b846d290c2b0ae
Xiangcheng Kuo <xiangcheng.guo@fastonetech.com>
2024-11-11 16:11:05 +0800

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import org.jetbrains.intellij.platform.gradle.utils.extensionProvider
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
@@ -29,12 +30,32 @@ java {
 group = "com.github.orange-guo.fill-kotlin-arguments"
 version = libs.versions.fill.kotlin.arguments.get()
 
+// https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-extension.html
 intellijPlatform {
 	pluginConfiguration {
+		id = "com.github.orange-guo.fill-kotlin-arguments"
+		name = "Fill Kotlin Arguments"
+		version = libs.versions.fill.kotlin.arguments.get()
+		vendor {
+			url = "https://github.com/orange-guo"
+			name = "orange-guo"
+		}
+		description = """
+<p>A JetBrains IDEA plugin used to fill kotlin arguments.</p>
+<a target="_blank" href="https://raw.githubusercontent.com/orange-guo/fill-kotlin-arguments/main/screencast.gif">
+<img src="https://raw.githubusercontent.com/orange-guo/fill-kotlin-arguments/main/screencast.gif">
+</a>
+</p>
+		""".trimIndent()
+		changeNotes = ""
 		ideaVersion {
 			// https://confluence.jetbrains.com/display/IDEADEV/Build+Number+Ranges
 			sinceBuild = "242"
-			untilBuild = "243.*"
+			// untilBuild = "243.*"
+			// The until-build attribute can be unset by setting provider { null } as a value. Note that passing only null will make Gradle use a default value instead.
+			provider {
+				null
+			}
 		}
 	}
 }
